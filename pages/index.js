b2c91ffb565e89d1a49c7e02557f9b8b4127525b
AdrianPaoletti <adriandelapao@gmail.com>
2022-04-27 17:59:56 +0200

    asdf

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useEffect } from "react";
-import { de } from "netlify-cms-locales";
 
 export default function Home() {
   useEffect(() => {
